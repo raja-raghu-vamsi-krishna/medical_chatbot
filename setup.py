f09e3a28de811ac916fb74575aeb0cf4efe163c5
raja-raghu-vamsi-krishna <131585236+raja-raghu-vamsi-krishna@users.noreply.github.com>
2024-01-19 19:01:23 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'adas754',
+    author_email= 'anweshadas754@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
